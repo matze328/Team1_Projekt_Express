c33ccad81fe265167dde2d6d1f783413bd26f3f3
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-06-11 12:29:29 +0200

    add docker
    - create docker branch
    - add docker + docker-compose

--- src/database/setup/database.js
+++ src/database/setup/database.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require("sequelize");
 
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
+
 // Connect to MySQL using Sequelize
-const radioSequelize = new Sequelize("radio_app", "root", "root1234", {
-  host: "localhost",
+const radioSequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "mysql",
 });
 
