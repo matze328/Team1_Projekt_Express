7cbb9370f877771ad6df0cb22bd1e35fc3b0987b
matze328 <152269645+matze328@users.noreply.github.com>
2024-09-26 06:12:55 +0200

    DynamoDB setup update
    - init Session Token
    - über env

--- src/database/dynamoDB/setup/index.js
+++ src/database/dynamoDB/setup/index.js
@@ -4,6 +4,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    token: process.env.AWS_SESSION_TOKEN,
     region: 'eu-central-1' 
 });
 
